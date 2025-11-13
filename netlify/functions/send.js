ae0768c2bf435a130fd62a0c43e3bf216d3d8387
Coder Agent Maaz <maaz582ss@gmail.com>
2025-11-13 19:39:41 +0530

    Update Netlify send.js with Resend API key and improvements

--- netlify/functions/send.js
+++ netlify/functions/send.js
@@ -1,6 +1,6 @@
 import { Resend } from 'resend';
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend(re_6i8eoTPq_Q4HH1JsVq3qS51rNLhsey6NY);
 
 export async function handler(event) {
   try {
